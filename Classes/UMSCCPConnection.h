630ad106333d7d50b0149ed41db41588881591b8
Andreas Fink <andreas@fink.org>
2016-10-28 08:57:30 +0200

    initial checkin

--- Classes/UMSCCPConnection.h
+++ Classes/UMSCCPConnection.h
@@ -5,6 +5,9 @@
 //  Created by Andreas Fink on 04/04/16.
 //  Copyright (c) 2016 Andreas Fink
 //
+// This source is dual licensed either under the GNU GENERAL PUBLIC LICENSE
+// Version 3 from 29 June 2007 and other commercial licenses available by
+// the author.
 
 #import <ulib/ulib.h>
 #import <ulibasn1/ulibasn1.h>
--- Classes/UMSCCP_mtpTransfer.m
+++ Classes/UMSCCP_mtpTransfer.m
@@ -5,6 +5,9 @@
 //  Created by Andreas Fink on 05.04.16.
 //  Copyright (c) 2016 Andreas Fink
 //
+// This source is dual licensed either under the GNU GENERAL PUBLIC LICENSE
+// Version 3 from 29 June 2007 and other commercial licenses available by
+// the author.
 
 #import "UMSCCP_mtpTransfer.h"
 #import "UMLayerSCCP.h"
