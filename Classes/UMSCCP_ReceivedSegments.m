4deca932832d8786889da07c35feec1359797333
Andreas Fink <andreas@fink.org>
2022-02-21 12:57:26 +0100

    wip

--- Classes/UMSCCP_ReceivedSegments.m
+++ Classes/UMSCCP_ReceivedSegments.m
@@ -100,8 +100,9 @@
 #ifdef SEGMENTATION_DEBUG
         NSLog(@"first packet = NO");
 #endif
-        current = s.max - s.segment.remainingSegment - 1;
-        
+        s.max = _max;
+        current = _max - s.segment.remainingSegment - 1;
+
 #ifdef SEGMENTATION_DEBUG
         NSLog(@"s.segment.remainingSegment = %d",s.segment.remainingSegment);
         NSLog(@"current = %d,s.max=%d, _max=%d",current,s.max,_max);
